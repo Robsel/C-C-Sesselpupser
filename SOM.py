8b3cc39553ee286859ecef75093343509366a424
Robsel <Robselsten@gmail.com>
2024-01-06 15:23:59 +0100

    rewrite som

--- SOM.py
+++ SOM.py
@@ -1,48 +1,54 @@
 import numpy as np
 
 class SOM:
-    def __init__(self, input_dim, som_dim=(10, 10), learning_rate=0.1, radius=None):
-        self.input_dim = input_dim
-        self.som_dim = som_dim
+    def __init__(self, data, num_neurons, epochs, learning_rate):
+        """
+        Constructor for the Self-Organizing Map (SOM).
+        
+        Parameters:
+        data (numpy.ndarray): The input data, shape (n, d)
+        num_neurons (int): Number of neurons in the SOM
+        max_epochs (int): Maximum number of epochs for training
+        learning_rate (float): Initial learning rate
+        """
+        self.data = data
+        self.num_neurons = num_neurons
+        self.epochs = epochs
         self.learning_rate = learning_rate
-        self.radius = radius if radius else max(som_dim) / 2
-        self.weights = np.random.random((som_dim[0], som_dim[1], input_dim))
-        self.iteration = 0
 
-    def _find_bmu(self, vector):
-        distances = np.linalg.norm(self.weights - vector, axis=2)
-        return np.unravel_index(distances.argmin(), distances.shape)
+    def train(self):
+        for counter in range(self.epochs):
+            for x in self.data:
+                # Find the neuron that is closest to the input data point
+                closest_neuron_idx = np.argmin(np.linalg.norm(self.weights - x, axis=1))
+                # Update the weights of the neuron to be closer to the input data point
+                self.weights[closest_neuron_idx] += self.learning_rate * (x - self.weights[closest_neuron_idx])
 
-    def _update_weights(self, vector, bmu):
-        # Calculate the distance from each neuron to the BMU
-        d = np.linalg.norm(np.array(list(np.ndindex(self.weights.shape[:2]))) - np.array(bmu), axis=1)
-        d = d.reshape(self.som_dim[0], self.som_dim[1])  # Reshape d to match the SOM dimensions
+    def map_data(self):
+        clusters = []
+        for x in self.data:
+            # Find the neuron that is closest to the input data point
+            closest_neuron_idx = np.argmin(np.linalg.norm(self.weights - x, axis=1))
+            clusters.append(closest_neuron_idx)
+        return clusters
 
-        # Compute the influence of the BMU on each neuron
-        influence = np.exp(-(d**2) / (2 * (self.radius**2)))
+    def SOMofAwesom(data):
+        num_neurons = max(data.shape[0] // 2, 4)
+        som = SOM(data, num_neurons, 10, 0.5)
+        som.train()
+        clusters = som.map_data()
+        arrayplus1 = np.hstack((data, np.array(clusters).reshape(-1, 1)))
 
-        # Learning rate adjustment (using broadcasting)
-        lr_adjusted = self.learning_rate * influence
+        return arrayplus1
 
-        # Update weights
-        for i in range(self.som_dim[0]):
-            for j in range(self.som_dim[1]):
-                self.weights[i, j, :] += lr_adjusted[i, j] * (vector - self.weights[i, j, :])
+    #def SOMofAwesom(array(n, d)):
 
-    def train(self, data, num_iterations):
-        for i in range(num_iterations):
-            for vector in data:
-                bmu = self._find_bmu(vector)
-                self._update_weights(vector, bmu)
-            self.iteration += 1
-            self.radius = max(self.som_dim) / 2 / (1 + i / (num_iterations / 2))
+        #SOMObj = SOM(array, int(max(array/2, 4)), 10, 0.5)
+            #select random input
+            #compute closest neuron
+            #update neuron and neighbors
+            #repeat for all input data
+            #determine clusters based on nodes and their proximity to data points
+            #save the clusters in a new arrayplus1(n, d+1), filled with array(n, d)
 
-    def map_vects(self, data):
-        mapped = np.array([self.weights[self._find_bmu(v)] for v in data])
-        return mapped.reshape(len(data), self.input_dim)
-
-# Example Usage
-input_data = np.random.rand(100, 5)  # 100 data points, each with 5 dimensions
-som = SOM(input_dim=5, som_dim=(10, 10))
-som.train(input_data, num_iterations=100)
-output_data = som.map_vects(input_data)
+        #return arrayplus1
